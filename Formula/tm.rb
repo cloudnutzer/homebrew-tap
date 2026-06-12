1eef3c9e7a1b44991898490ed5a349777f1b3022
Eugen Hinz <92438750+cloudnutzer@users.noreply.github.com>
2026-06-12 21:59:37 +0200

    Add tm formula (v0.1.0)

---
+++ Formula/tm.rb
@@ -0,0 +1,61 @@
+class Tm < Formula
+  include Language::Python::Virtualenv
+
+  desc "Interactive terminal UI for managing tmux sessions"
+  homepage "https://github.com/cloudnutzer/tm"
+  url "https://github.com/cloudnutzer/tm/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "f8c7df9bc6707c8761af3f385fa69ff683bfbbaef5dfa35cf3ed4e68310d063d"
+  license "MIT"
+
+  depends_on "python@3.14"
+  depends_on "tmux"
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/06/ff/7841249c247aa650a76b9ee4bbaeae59370dc8bfd2f6c01f3630c35eb134/markdown_it_py-4.2.0.tar.gz"
+    sha256 "04a21681d6fbb623de53f6f364d352309d4094dd4194040a10fd51833e418d49"
+  end
+
+  resource "mdit-py-plugins" do
+    url "https://files.pythonhosted.org/packages/59/fc/f8d0863f8862f25602c0404d75568e89fb6b4109804645e5cdfb1be5cf56/mdit_py_plugins-0.6.1.tar.gz"
+    sha256 "a2bca0f039f39dbd35fb74ae1b5f998608c437463371f0ff7f49a19a17a114d0"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/d7/47/e4501f49c178ae1d9f4a75073fda4204f52647993f075a9db4d14930e0c5/platformdirs-4.10.0.tar.gz"
+    sha256 "31e761a6a0ca04faf7353ea759bdba55652be214725111e5aac52dfa29d4bef7"
+  end
+
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/c3/b2/bc9c9196916376152d655522fdcebac55e66de6603a76a02bca1b6414f6c/pygments-2.20.0.tar.gz"
+    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/c0/8f/0722ca900cc807c13a6a0c696dacf35430f72e0ec571c4275d2371fca3e9/rich-15.0.0.tar.gz"
+    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
+  end
+
+  resource "textual" do
+    url "https://files.pythonhosted.org/packages/9b/7a/c519db0aba5024f86e71e9631810bfdd6866ed2c8695bd7fa34b90e7ef59/textual-8.2.7.tar.gz"
+    sha256 "658f568ff81e30ed43890c3e07520390e5cf1b4763822006e060656b0a88f105"
+  end
+
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/72/94/1a15dd82efb362ac84269196e94cf00f187f7ed21c242792a923cdb1c61f/typing_extensions-4.15.0.tar.gz"
+    sha256 "0cea48d173cc12fa28ecabc3b837ea3cf6f38c6d1136f85cbaaf598984861466"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    man1.install "docs/tm.1"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tm --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# homebrew-tap
+
+Personal Homebrew tap.
+
+```bash
+brew install cloudnutzer/tap/tm
+```
+
+| Formula | Description |
+|---|---|
+| [`tm`](https://github.com/cloudnutzer/tm) | Interactive terminal UI for managing tmux sessions |
